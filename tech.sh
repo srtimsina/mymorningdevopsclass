1687a1f7293a782931a0193e7278328f067cf764
Suryaraj Timsina <devopsuryaraj@gmail.com>
2023-06-07 01:57:50 +0000

    Added run.sh script

--- tech.sh
+++ tech.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will install wordpress
-
+echo "Now the wordpress should be working"
--- test.sh
+++ test.sh
@@ -7,3 +7,4 @@ wget https://www.tooplate.com/zip-templates/2129_crispy_kitchen.zip
 unzip -o 2129_crispy_kitchen.zip
 cp -r 2129_crispy_kitchen/* /var/www/html/
 systemctl restart httpd
+systemctl status httpd
